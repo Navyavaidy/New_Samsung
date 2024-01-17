379fd851abc5d7b1d515228a4de271f7614ce6fd
Navyavaidy <navyavaidyanathan1999@gmail.com>
2024-01-17 18:16:51 +0530

    update webpage

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1932389.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 162912288, 162912289, 162912290, 162912291, 162976160, 163347056, 163461208, 163484716, 163486699, 163827407, 163970530 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/162912312/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/162912313/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/162912311/1/ammo.js', 'preload' : true},
+];
